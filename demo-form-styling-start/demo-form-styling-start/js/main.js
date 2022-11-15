12f4ca70e8ff8f498cd1a6aa4adcbb592bafeea8
Matthew Suddaby <msuddaby2@nait.ca>
2022-11-15 16:48:23 -0700

    finish for the day

--- demo-form-styling-start/demo-form-styling-start/js/main.js
+++ demo-form-styling-start/demo-form-styling-start/js/main.js
@@ -1,3 +1,14 @@
-document.querySelector('.toggle-btn').addEventListener('click', () => {
-    document.querySelector('nav').classList.toggle('show-nav');
-});
+// document.querySelector('.toggle-btn').addEventListener('click', () => {
+//     document.querySelector('nav').classList.toggle('show-nav');
+// });
+
+var item = document.querySelector('#bruh');
+let move = 40;
+
+
+
+document.querySelector('#bruh').addEventListener('mouseover', () => {
+    move += 40;
+    item.style.left = `${move}px`;
+    item.style.bottom = `${move}px`;
+})
